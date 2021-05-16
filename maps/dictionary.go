448248d6c2988353366c3f4d1c9e62fa4102b951
Sukant <sukant.iitr@gmail.com>
2021-05-16 18:36:12 +0530

    add new ord

---
+++ maps/dictionary.go
@@ -0,0 +1,19 @@
+package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word in the dictionary")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
